959def4b0bedc5fc29eff8eebb723534f0358344
tim <timaz98@ya.ru>
2022-08-10 17:12:27 +0500

    cache: add mutex

--- generator/app/cache/cache.go
+++ generator/app/cache/cache.go
@@ -1,13 +1,21 @@
 package cache
 
+import "sync"
+
 type Cache[T any] interface {
-	Add(at T)
+	Add(v T)
 	Values() []T
+}
+
+type Cache1 interface {
+	Add(v int)
+	Values() []int
 }
 
 var _ Cache[any] = (*LimitedCache[any])(nil)
 
 type LimitedCache[T any] struct {
+	mu    sync.RWMutex
 	index int
 	s     []T
 }
@@ -20,6 +28,8 @@ func NewLimitedCache[T any](size uint64) *LimitedCache[T] {
 }
 
 func (l *LimitedCache[T]) Add(v T) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
 	_ = append(l.s[:l.index], v)
 	if len(l.s) != cap(l.s) {
 		l.s = l.s[:l.index+1]
@@ -31,5 +41,7 @@ func (l *LimitedCache[T]) Add(v T) {
 }
 
 func (l *LimitedCache[T]) Values() []T {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
 	return l.s[:]
 }
--- generator/app/cache/cache_test.go
+++ generator/app/cache/cache_test.go
@@ -1,7 +1,6 @@
 package cache
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -11,30 +10,23 @@ func TestLimitedCache(t *testing.T) {
 	c := NewLimitedCache[int](3)
 
 	c.Add(1)
-	fmt.Println(c.Values())
 	require.ElementsMatch(t, c.Values(), []int{1})
 
 	c.Add(2)
-	fmt.Println(c.Values())
 	require.ElementsMatch(t, c.Values(), []int{1, 2})
 
 	c.Add(3)
-	fmt.Println(c.Values())
 	require.ElementsMatch(t, c.Values(), []int{1, 2, 3})
 
 	c.Add(4)
-	fmt.Println(c.Values())
 	require.ElementsMatch(t, c.Values(), []int{4, 2, 3})
 
 	c.Add(5)
-	fmt.Println(c.Values())
 	require.ElementsMatch(t, c.Values(), []int{4, 5, 3})
 
 	c.Add(6)
-	fmt.Println(c.Values())
 	require.ElementsMatch(t, c.Values(), []int{4, 5, 6})
 
 	c.Add(7)
-	fmt.Println(c.Values())
 	require.ElementsMatch(t, c.Values(), []int{7, 5, 6})
 }
